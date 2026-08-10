80c82efa526d37774a5ebe56f78a130d9ceff60f
YsDeveloper <hello@pixelyug.com>
2026-08-10 23:34:04 +0530

    product adding and updating remove

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,84 +1,5 @@
 import ProductsModel from "../models/products.model.js";
 
-const CreateProducts = async (req, res) => {
-  const { title, description, price, stock, rating, image, category } =
-    req.body;
-
-  if (!title || !price || !image || !category) {
-    return res
-      .status(401)
-      .json({ msg: "Invalid required details", success: false });
-  }
-
-  try {
-    const CreateProduct = await ProductsModel.create({
-      title,
-      description,
-      price,
-      stock,
-      image,
-      rating,
-      category,
-    });
-    return res.status(201).json({
-      msg: "Product create successfully",
-      success: true,
-      CreateProduct,
-    });
-  } catch (error) {
-    console.error("error : ", error);
-    return res
-      .status(500)
-      .json({ msg: "Server side error", error, success: false });
-  }
-};
-
-const UpdateProduct = async (req, res) => {
-  try {
-    const updatedProduct = await ProductsModel.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
-
-    if (!updatedProduct) {
-      return res.status(404).json({
-        msg: "Product not found",
-        success: false,
-      });
-    }
-
-    return res.status(200).json({
-      msg: "Product updated successfully",
-      success: true,
-      product: updatedProduct,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: "Server side error",
-      success: false,
-      error: error.message,
-    });
-  }
-};
-
-const DeleteProdct = async (req, res) => {
-  try {
-    await ProductsModel.findByIdAndDelete(req.params.id);
-
-    return res
-      .status(200)
-      .json({ msg: "Product delete successfully", success: true });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ msg: "server side error", error, success: false });
-  }
-};
-
 const GetAllProduct = async (req, res) => {
   const { category, sortBy } = req.query;
 
@@ -132,22 +53,17 @@ const SearchProduct = async (req, res) => {
       title: { $regex: search, $options: "i" },
     });
 
-    return res
-      .status(200)
-      .json({
-        msg: "Search products",
-        success: true,
-        search_result: SearchedProduct,
-      });
+    return res.status(200).json({
+      msg: "Search products",
+      success: true,
+      search_result: SearchedProduct,
+    });
   } catch (error) {
     return res.status(500).json({ msg: "Server side error", error });
   }
 };
 
 export default {
-  CreateProducts,
-  UpdateProduct,
-  DeleteProdct,
   GetAllProduct,
   ParticularProducts,
   SearchProduct,
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,9 +3,6 @@ import ProuctController from '../controllers/product.controller.js'
 
 const productRoute = express.Router()
 
-productRoute.post('/create' ,ProuctController.CreateProducts)
-productRoute.put('/update/:id' ,ProuctController.UpdateProduct)
-productRoute.delete('/delete/:id' ,ProuctController.DeleteProdct)
 productRoute.get('/get' ,ProuctController.GetAllProduct)
 productRoute.get('/get/particular_product/:id' ,ProuctController.ParticularProducts)
 productRoute.get('/search_product' ,ProuctController.SearchProduct)
